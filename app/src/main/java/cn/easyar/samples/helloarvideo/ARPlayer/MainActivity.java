73be8283501fa0c46cf19455738a88b9a30b124f
A-celer <764884989@qq.com>
2017-05-03 00:51:20 +0800

    First commit

---
+++ app/src/main/java/cn/easyar/samples/helloarvideo/ARPlayer/MainActivity.java
@@ -0,0 +1,100 @@
+/**
+* Copyright (c) 2015-2016 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
+* EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
+* and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
+*/
+
+package cn.easyar.samples.helloarvideo.ARPlayer;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import cn.easyar.engine.EasyAR;
+import cn.easyar.samples.helloarvideo.R;
+
+public class MainActivity extends AppCompatActivity{
+
+    /*
+    * Steps to create the key for this sample:
+    *  1. login www.easyar.com
+    *  2. create app with
+    *      Name: HelloARVideo
+    *      Package Name: cn.easyar.samples.helloarvideo
+    *  3. find the created item in the list and show key
+    *  4. set key string bellow
+    */
+    static String key = "6dcc8f7e2cafc99e6f87bf992482c161KAms2D5XbhrpBeTvLiq3sDo2l2sdJ08oUgUoisOXVqFv0paqnm8xBVPj6goQxB0IteCdccDNSmvR1hebtVmmxd7GNaY82oXZw4pXq2Huat7hhN5UZO8kBgNyc6mAVuhublC8TIzUDO0cKlgt5wM1j97wuuvY2fLkJ9pFfvRO";
+
+    static {
+        System.loadLibrary("EasyAR");
+        System.loadLibrary("HelloARVideoNative");
+    }
+
+    public static native void nativeInitGL();
+    public static native void nativeResizeGL(int w, int h);
+    public static native void nativeRender();
+    private native boolean nativeInit();
+    private native void nativeDestory();
+    private native void nativeRotationChange(boolean portrait);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        EasyAR.initialize(this, key);
+        nativeInit();
+
+        GLView glView = new GLView(this);
+        glView.setRenderer(new Renderer());
+        glView.setZOrderMediaOverlay(true);
+
+        ((ViewGroup) findViewById(R.id.preview)).addView(glView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        nativeRotationChange(getWindowManager().getDefaultDisplay().getRotation() == android.view.Surface.ROTATION_0);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration config) {
+        super.onConfigurationChanged(config);
+        nativeRotationChange(getWindowManager().getDefaultDisplay().getRotation() == android.view.Surface.ROTATION_0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        nativeDestory();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EasyAR.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EasyAR.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
